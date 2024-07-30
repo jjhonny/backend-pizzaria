10b49ba3a1f7122f7be1ab23153de45b0173019a
jjhonny <ojhonny1@hotmail.com>
2024-07-30 15:04:21 -0400

    feat: create user

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma";
+
 interface UserRequest {
   name: string;
   email: string;
@@ -6,9 +8,36 @@ interface UserRequest {
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    console.log(name);
+    // verificar se ele enviou um email
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
 
-    return { name: name };
+    // verificar se esse email já está cadastrado
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 
