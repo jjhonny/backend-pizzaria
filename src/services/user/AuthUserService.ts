332a1b44197b0d47ff37a285f881e323907ff11d
jjhonny <ojhonny1@hotmail.com>
2024-07-31 14:52:09 -0400

    feat: generates user token

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -26,7 +27,25 @@ class AuthUserService {
       throw new Error("User/password incorrect");
     }
 
-    return { ok: true };
+    // Se deu tudo certo vamos gerar o token para o usuario
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
