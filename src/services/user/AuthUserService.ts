60763b1d7105eddadad031940751939168113712
jjhonny <ojhonny1@hotmail.com>
2024-07-31 14:08:36 -0400

    feat: create endpoit auth user

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,33 @@
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    // Verificar se o email existe
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User/password incorrect");
+    }
+
+    // Verificar se a senha que ele mandou está correta.
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("User/password incorrect");
+    }
+
+    return { ok: true };
+  }
+}
+
+export { AuthUserService };
