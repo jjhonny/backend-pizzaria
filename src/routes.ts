459403f2f85573c55abeb66ab627c03663b4cd79
jjhonny <ojhonny1@hotmail.com>
2024-08-07 08:48:13 -0400

    feat: remove item from table

--- src/routes.ts
+++ src/routes.ts
@@ -15,6 +15,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoverOrderController } from "./controllers/order/RemoverOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 const router = Router();
 
@@ -50,5 +51,10 @@ router.get(
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoverOrderController().handle);
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 
 export { router };
