26b3cf85d5f164c02c0ca921305ef48029658a21
jjhonny <ojhonny1@hotmail.com>
2024-08-07 10:04:10 -0400

    feat: get table order details

--- src/routes.ts
+++ src/routes.ts
@@ -18,6 +18,7 @@ import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrderController } from "./controllers/order/ListOrderController";
+import { DetailOrderController } from "./controllers/order/DetailOrderController";
 
 const router = Router();
 
@@ -60,5 +61,10 @@ router.delete(
 );
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 router.get("/orders", isAuthenticated, new ListOrderController().handle);
+router.get(
+  "/order/detail",
+  isAuthenticated,
+  new DetailOrderController().handle
+);
 
 export { router };
